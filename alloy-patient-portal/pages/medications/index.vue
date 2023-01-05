f7b4dd800cb7b739bf47017be4ddbbe2433d2512
Robert Floyd <rbrt@hey.com>
2023-01-04 20:31:01 -0500

    Update table and link

--- alloy-patient-portal/pages/medications/index.vue
+++ alloy-patient-portal/pages/medications/index.vue
@@ -27,8 +27,6 @@
                       Name
                     </th>
                     <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
-                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
-                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role</th>
                     <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
                       <span class="sr-only">Edit</span>
                     </th>
@@ -37,18 +35,16 @@
                 <tbody class="divide-y divide-gray-200 bg-white">
                   <tr v-for="medication in medications" :key="medication.id">
                     <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-                      <div class="flex items-center">
-                        <div class="h-10 w-10 flex-shrink-0"></div>
-                        <div class="ml-4">
-                          <div class="font-medium text-gray-900">{{ medication.medication_name }}</div>
-                        </div>
-                      </div>
+                      <div class="font-medium text-gray-900">{{ medication.medication_name }}</div>
                     </td>
                     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                       <span
                         class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800"
                         >Active</span
                       >
+                    </td>
+                    <td>
+                      <NuxtLink :to="'/medications' + '/' + medication.id">View Medication</NuxtLink>
                     </td>
                   </tr>
                 </tbody>
