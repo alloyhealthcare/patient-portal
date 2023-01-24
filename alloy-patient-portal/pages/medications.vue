9758f7ebc431c82a44cf12327b59d2bce9d853af
Robert Floyd <rbrt@hey.com>
2023-01-23 20:09:36 -0500

    Update text

--- alloy-patient-portal/pages/medications.vue
+++ alloy-patient-portal/pages/medications.vue
@@ -4,9 +4,7 @@
       <div class="sm:flex sm:items-center">
         <div class="sm:flex-auto">
           <h1 class="text-xl font-semibold text-gray-900">Your Medications</h1>
-          <p class="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title, email and role.
-          </p>
+          <p class="mt-2 text-sm text-gray-700">A list of all the medications in your record.</p>
         </div>
         <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
           <button
